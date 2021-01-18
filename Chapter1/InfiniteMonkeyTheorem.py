2c461ce9e8586223573037b3c9df2ee1942427bf
tryphenaP <70466428+tryphenaP@users.noreply.github.com>
2021-01-18 13:38:15 -0800

    Update InfiniteMonkeyTheorem.py

--- Chapter1/InfiniteMonkeyTheorem.py
+++ Chapter1/InfiniteMonkeyTheorem.py
@@ -1,1 +1,15 @@
+import random
+import string 
 
+def generate_28_random_characters(string_to_generate):
+  length_of_string_to_generate=len(string_to_generate)
+
+def comapre_two_string(string_generated,string_to_generate): 
+  string_generated == string_to_generate if (string_generated == string_to_generate) print ("problem solved") else repeat_until_stringGenerated_equals_stringToGenerate()
+
+def repeat_until_stringGenerated_equals_stringToGenerate(): 
+  generate_28_random_characters(string_to_generate)
+  
+for i in range(0, length_of_string_to_generate): 
+    string_generated=[''.join(random.choice(string.ascii)+'')] 
+    comapre_two_string(string_generated,string_to_generate )
